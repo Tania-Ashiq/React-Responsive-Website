e97d7f90990b35dafe3c13fd0a112c9454d0375a
Tania Ashiq <taniaashiq45@gmail.com>
2024-09-11 22:30:26 -0700

    Real

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import Header from "./Components/Header/header";
+import Hero from "./Components/Hero/hero";
+import "./App.css";
+import Companies from "./Components/Companies/Companies";
+import Residencies from "./Components/Residencies/Residencies";
+import Value from "./Components/value/value";
+import Contact from "./Components/Contact/Contact";
+import GetStarted from "./Components/GetStarted/GetStarted";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient"> </div>
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies />
+      <Value />
+      <Contact />
+      <GetStarted />
+      <Footer />
+    </div>
   );
 }
 
