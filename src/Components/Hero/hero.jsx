59ddf1549ec023f80bedc674d57dda9e72912f22
Tania Ashiq <taniaashiq45@gmail.com>
2024-09-12 00:14:21 -0700

    Update hero.jsx

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -33,7 +33,6 @@ const hero = () => {
             </div>
             <div className="flexCenter search-bar">
               <HiLocationMarker color="var(--blue)" size={25} />
-              <input type="text" />
               <button className="button">Search</button>
             </div>
 
